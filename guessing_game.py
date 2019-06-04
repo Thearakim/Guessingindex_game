7fbf5816b8c23bcb223a6f39c44a55bef733ba44
Theara Kim <45727527+Thearakim@users.noreply.github.com>
2019-06-04 10:57:48 +0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -2,7 +2,7 @@ secret_number = 9
 guess_number = 0
 guess_limit = 2
 while guess_number < guess_limit:
-    guess = input("Enter number to guess :")
+    guess = int(input("Enter number to guess :"))
     guess_number +=1
     if guess == secret_number:
         print("Bingo you are win!!!")
